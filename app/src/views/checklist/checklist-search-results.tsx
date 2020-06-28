9a6584bc1fd908990c4932480b0af6548700e980
H1net <ben@df-network.com>
2020-06-28 21:49:36 +0100

    Work on ant table typescript and add icons

---
+++ app/src/views/checklist/checklist-search-results.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import { Table } from 'antd'
+import { ColumnsType } from 'antd/es/table'
+
+interface Checklist {
+  key: number
+  name: string
+}
+
+const columns: ColumnsType<Checklist>[] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    width: 100,
+    fixed: 'left',
+    filters: [
+      {
+        text: 'Joe',
+        value: 'Joe',
+      },
+      {
+        text: 'John',
+        value: 'John',
+      },
+    ],
+    onFilter: (value: any, record: { name: string | any[] }) => record.name.indexOf(value) === 0,
+  },
+  {
+    title: 'Other',
+    children: [
+      {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+        width: 150,
+        sorter: (a: { age: number }, b: { age: number }) => a.age - b.age,
+      },
+      {
+        title: 'Address',
+        children: [
+          {
+            title: 'Street',
+            dataIndex: 'street',
+            key: 'street',
+            width: 150,
+          },
+          {
+            title: 'Block',
+            children: [
+              {
+                title: 'Building',
+                dataIndex: 'building',
+                key: 'building',
+                width: 100,
+              },
+              {
+                title: 'Door No.',
+                dataIndex: 'number',
+                key: 'number',
+                width: 100,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Company',
+    children: [
+      {
+        title: 'Company Address',
+        dataIndex: 'companyAddress',
+        key: 'companyAddress',
+        width: 200,
+      },
+      {
+        title: 'Company Name',
+        dataIndex: 'companyName',
+        key: 'companyName',
+      },
+    ],
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender',
+    width: 80,
+    fixed: 'right',
+  },
+]
+
+const data: any[] | undefined = []
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i,
+    name: 'John Brown',
+    age: i + 1,
+    street: 'Lake Park',
+    building: 'C',
+    number: 2035,
+    companyAddress: 'Lake Street 42',
+    companyName: 'SoftLake Co',
+    gender: 'M',
+  })
+}
+
+export const Checklists = () => {
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      bordered
+      size="middle"
+      scroll={{ x: 'calc(700px + 50%)', y: 240 }}
+    />
+  )
+}
